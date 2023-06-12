7ef4c38b02ff161d2e3ac99c59c737e38fb6d2d2
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-06-12 14:53:12 +0200

    Edit App.js

--- sportsee_p12/src/App.js
+++ sportsee_p12/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Error from './pages/Error'
+import Header from './components/Header'
+import Sidebar from './components/SideBar'
+import SourceProvider from './utils/context'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <div className="mainContent">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Navigate to="user/12" />} />
+          <Route
+            path="user/:userId"
+            element={
+              <SourceProvider>
+                <Dashboard />
+              </SourceProvider>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
